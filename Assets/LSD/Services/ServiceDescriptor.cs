b39cfbbc2cc509ac73b2464ac14909f427811b6a
Sweet Acid <antoniu.ilea@gmail.com>
2021-09-08 11:24:11 +0300

    Cleaned up descriptors

--- Assets/LSD/Services/ServiceDescriptor.cs
+++ Assets/LSD/Services/ServiceDescriptor.cs
@@ -9,32 +9,19 @@ namespace LSD
 
         public ServiceLifetime Lifetime { get; }
         public object Implementation { get; internal set; }
-        public Type ServiceType { get; }
-        public Type ImplementationType { get; }
         public Func<object> GetInstance { get; internal set; }
         public Func<IEnumerable<Override>, object> GetOverridenInstance { get; internal set; }
 
-        public ServiceDescriptor(object impl, Type serviceType = null)
+        public ServiceDescriptor(object impl)
         {
-            ImplementationType = ServiceType = impl.GetType();
-            if (serviceType != null)
-                ServiceType = serviceType;
-
             Implementation = impl;
             Lifetime = ServiceLifetime.Singleton;
         }
 
-        public ServiceDescriptor(Type serviceType, Type implementationType)
-        {
-            ServiceType = serviceType;
-            ImplementationType = implementationType;
-            Implementation = null;
-            Lifetime = ServiceLifetime.Transient;
-        }
-
         public ServiceDescriptor()
         {
-
+            Implementation = null;
+            Lifetime = ServiceLifetime.Transient;
         }
     }
 }
