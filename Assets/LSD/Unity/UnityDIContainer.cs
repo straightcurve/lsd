793c72853fa9011657fad17665ee1ccbe3eed773
Sweet Acid <antoniu.ilea@gmail.com>
2021-09-08 11:03:59 +0300

    Extracted UnityRegistration to its own file

--- Assets/LSD/Unity/UnityDIContainer.cs
+++ Assets/LSD/Unity/UnityDIContainer.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using LSD.Unity.Creation;
 using UnityEngine;
 
 namespace LSD.Unity
@@ -22,47 +18,6 @@ namespace LSD.Unity
             var reg = new UnityRegistration<TService, TImpl>(syringe);
             collection.Add(typeof(TService), reg.Descriptor);
             return reg;
-        }
-
-        public class UnityRegistration<TService, TImpl> : Registration<TService, TImpl>, IUnitySourceSelection<TImpl> where TImpl : MonoBehaviour
-        {
-            public UnityRegistration(ISyringe syringe) : base(syringe)
-            {
-            }
-
-            public new ILifetimeSelectionStage FromNew()
-            {
-                var type = typeof(TImpl);
-                if (!type.IsSubclassOf(typeof(MonoBehaviour)))
-                    return base.FromNew();
-
-                strategy = new ComponentStrategy(Syringe);
-
-                Descriptor.GetInstance = () => strategy.Create(type);
-                Descriptor.GetOverridenInstance = (IEnumerable<Override> overrides) => strategy.Create(type, overrides);
-
-                return this;
-            }
-
-            public ILifetimeSelectionStage FromNewComponent()
-            {
-                return FromNew();
-            }
-
-            public ILifetimeSelectionStage FromPrefab(TImpl prefab)
-            {
-                var type = typeof(TImpl);
-                strategy = new PrefabStrategy(Syringe, prefab);
-                Descriptor.GetInstance = () => strategy.Create(type);
-                return this;
-            }
-        }
-
-        public interface IUnitySourceSelection<TImpl> : ISourceSelectionStage<TImpl>
-        {
-            [Obsolete("Use FromNew() instead")]
-            ILifetimeSelectionStage FromNewComponent();
-            ILifetimeSelectionStage FromPrefab(TImpl prefab);
         }
     }
 }
---
+++ Assets/LSD/Unity/UnityRegistration.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using LSD.Unity.Creation;
+using UnityEngine;
+
+namespace LSD.Unity
+{
+    public interface IUnitySourceSelection<TImpl> : ISourceSelectionStage<TImpl>
+    {
+        [Obsolete("Use FromNew() instead")]
+        ILifetimeSelectionStage FromNewComponent();
+        ILifetimeSelectionStage FromPrefab(TImpl prefab);
+    }
+
+    public class UnityRegistration<TService, TImpl> : Registration<TService, TImpl>, IUnitySourceSelection<TImpl> where TImpl : MonoBehaviour
+    {
+        public UnityRegistration(ISyringe syringe) : base(syringe)
+        {
+        }
+
+        public new ILifetimeSelectionStage FromNew()
+        {
+            var type = typeof(TImpl);
+            if (!type.IsSubclassOf(typeof(MonoBehaviour)))
+                return base.FromNew();
+
+            strategy = new ComponentStrategy(Syringe);
+
+            Descriptor.GetInstance = () => strategy.Create(type);
+            Descriptor.GetOverridenInstance = (IEnumerable<Override> overrides) => strategy.Create(type, overrides);
+
+            return this;
+        }
+
+        public ILifetimeSelectionStage FromNewComponent()
+        {
+            return FromNew();
+        }
+
+        public ILifetimeSelectionStage FromPrefab(TImpl prefab)
+        {
+            var type = typeof(TImpl);
+            strategy = new PrefabStrategy(Syringe, prefab);
+            Descriptor.GetInstance = () => strategy.Create(type);
+            return this;
+        }
+    }
+}
